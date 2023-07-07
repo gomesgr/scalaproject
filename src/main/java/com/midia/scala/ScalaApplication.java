762f87976565fdcf2028f5803cab4abb615593ac
Gabriel <gomes.g.r@outlook.com>
2023-07-06 23:22:17 -0300

    initial commit

---
+++ src/main/java/com/midia/scala/ScalaApplication.java
@@ -0,0 +1,13 @@
+package com.midia.scala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalaApplication.class, args);
+	}
+
+}
